5f1db2eaf45b9a5f87fdba8e9a7370ee06963ac5
hadiya8149 <hadiya8149@gmail.com>
2020-11-20 19:41:59 +0500

    Update personalDiary.py

--- personalDiary.py
+++ personalDiary.py
@@ -1,73 +1,41 @@
 
 from tkinter import *
 from datetime import date
-
-root = Tk()
-root.title('Personal Diary')
-canvas = Canvas(root)
-canvas.pack(side=LEFT, fill = BOTH,expand =True)
-canvas['bg'] = 'light blue'
-canvas.configure(canvas, bd=0, width=500, height=500)
-
-Label(canvas, height=5, text='Hi sweetheart How was your day?', background='light blue', foreground='black').place(x=150, y=1)
-yCordinate = 150
-
-for i in range(0, 5): 
-    Label(canvas, height =0, text='________________________________________________________', background='light blue', foreground = 'blue').place(x=50, y= yCordinate)
-    yCordinate+=50
-
-
-lbl_1 =  Entry(canvas, width = 65, bd=0, font="Saira 8 ", relief = "groove",highlightcolor = 'light blue', highlightbackground= 'light blue', background = 'light blue')
-lbl_2 =  Entry(canvas, width = 65, bd=0, font="Saira 8 ", relief = "groove",highlightcolor = 'light blue', highlightbackground= 'light blue', background = 'light blue')
-lbl_3 =  Entry(canvas, width = 65, bd=0, font="Saira 8 ", relief = "groove",highlightcolor = 'light blue', highlightbackground= 'light blue', background = 'light blue')
-lbl_4 =  Entry(canvas, width = 65, bd=0, font="Saira 8 ", relief = "groove",highlightcolor = 'light blue', highlightbackground= 'light blue', background = 'light blue')
-lbl_5 =  Entry(canvas, width = 65, bd=0, font="Saira 8 ", relief = "groove",highlightcolor = 'light blue', highlightbackground= 'light blue', background = 'light blue')
-lbl_1.place(x=50, y=147)
-lbl_2.place(x=50, y=197)
-lbl_3.place(x=50, y=247)
-lbl_4.place(x=50, y=297)
-lbl_5.place(x=50, y=347)
-diary_notes = {}
-
 date = date.today()
-def save_note():
-    text = lbl_1.get() + '\n' + lbl_2.get() + '\n' + lbl_3.get() + '\n' + lbl_4.get() + '\n' + lbl_5.get()
-    diary_notes.update({date.isoformat(): text})
-    print(diary_notes)
-    diarytext = open('diary.txt', 'a')
-    with diarytext as f:
-        f.write(str(diary_notes))
-        f.write('\n ')
-        f.close()
-    return text
-
-save = Button(canvas, text="Save", command=save_note)
-save.place(x=309, y=391)
-popup = Label(canvas, background='light blue', foreground = 'white', height = 0, text= 'Note saved',font = 'Arial 24')
 
 
-def show_history():
-    history_window = Toplevel(root)
-    history_window.title('All notes')
-    history_window['bg'] = 'light blue'
-    diary_text = open('diary.txt', 'r')
-    with diary_text as f:
-        data = f.read()
-    txt = Label(history_window, text=data , background = 'light blue', foreground ='black')
-    txt.pack()
+def unlock_app():
+    usrname = username.get()
+    passcode = password.get()
+    if  (usrname == 'hadiya' ) and (passcode== 'abcd')  :
+        root.destroy()
+        import homepage
+    else:
+        print(usrname)
+        print(passcode)
+        username.delete('0', 'end')
+        password.delete('0', 'end')
+    
+    
+root = Tk()
+root.title('Personal Diary -login page')
+root.geometry('200x200')
+log_page = Canvas(root)
+log_page['bg'] = 'light blue'
+log_page.pack(side=LEFT, fill=BOTH, expand=True)
 
-history = Button(canvas, text="History", command=show_history)
-history.place(x=10, y=10)
+log_page.config(log_page, height=500, width=500, bd=0)
 
-def callback(event):
+Label(log_page,height = 3, text="Username", background='light blue', foreground='black').grid(row=0, column=0)
 
-    def hide():
-        popup.place_forget()
-    def display():
-        popup.place(x=250, y= 391)
-        save.after(500, hide)
-    save.after(250, display)
+username = Entry(log_page, width = 10)
+username.grid(row=0, column =1)
+Label(log_page, height=3, text="Password", background ='light blue', foreground='black').grid(row=1, column=0)
+password = Entry(log_page, width=10, show='*')
+password.grid(row=1, column=1)
 
+login = Button(log_page, text="Login", command=unlock_app)
+login.grid(row=2, column=0)
 
-save.bind('<Button-1>', callback)
-canvas.mainloop()
+log_page.mainloop()
+
